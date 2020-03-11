f4a87c95c2868c26a23a00ff7cd2b9b14cd324ed
Zhu XH <zhuxiaohe95@163.com>
2020-03-11 22:00:01 +0800

    0.0.2.2 五子棋下子、输出棋盘 每日杂谈 2020-3-11 Maven相关文档

--- codeTestFieldMain/src/test/java/cn/zeromax/goBang/GoBangTest.java
+++ codeTestFieldMain/src/test/java/cn/zeromax/goBang/GoBangTest.java
@@ -1,7 +1,5 @@
 package cn.zeromax.goBang;
 
-import javafx.util.Pair;
-
 import java.util.LinkedList;
 
 public class GoBangTest {
@@ -9,13 +7,28 @@ public class GoBangTest {
     private int roundCount; // 回合数，记录第几回合
     private int empty; // 空余空格数量
     private int gameStatus; // 0 未开局, 1 黑方行动, 2 白方行动, -1 黑方胜利, -2 白方胜利, -3 平局 TODO: 改造成Enum? 试试状态模式?
-    private LinkedList<Pair<Integer, Integer>> moveStack; // 落子记录
+    private LinkedList<int[]> moveStack; // 落子记录 格式为[color, x, y]
 
     GoBangTest() {// ToDo: 试着写个单例?
         board = new int[19][19]; // 19 * 19 棋盘
         roundCount = 0;
         empty = 19 * 19;
         gameStatus = 0;
+        moveStack = new LinkedList<>();
+    }
+
+    /**
+     * 下子
+     * @param x 横坐标
+     * @param y 纵坐标
+     */
+    public void move(int x, int y) {
+        int color = 1;
+        if (gameStatus == 1 || gameStatus == 2) {
+            color = gameStatus;
+        }
+        board[x][y] = color;
+        moveStack.add(new int[]{color, x, y});
     }
 
     /**
@@ -95,7 +108,44 @@ public class GoBangTest {
         return Integer.MIN_VALUE; // TODO: 改成异常
     }
 
-    public static void main(String[] args) {
+    /**
+     * 输出棋盘目前状态
+     */
+    public void printBoard(){
+        //第一行 写明列号
+        System.out.print("  ");
+        for (int i = 0; i < 19; i++) {
+            System.out.print("  ");
+            if(i<9){
+                System.out.print(" ");
+            }
+            System.out.print(i+1);
+        }
+        System.out.println();
 
+        //第二行之后的 写明行号和棋盘情况
+        for (int i = 0; i < 19; i++) {
+            if(i<9){
+                System.out.print(" ");
+            }
+            System.out.print((i+1)+" ");
+            for (int j = 0; j < 19; j++) {
+                System.out.print("| ");
+                if(board[i][j]==0){
+                    System.out.print("  ");
+                }else if(board[i][j]==1){
+                    System.out.print("X "); // X 代表 1 黑棋
+                }else if(board[i][j]==2){
+                    System.out.print("O "); // O 代表 2 白棋
+                }
+            }
+            System.out.println(" |");
+        }
+    }
+
+    public static void main(String[] args) {
+        GoBangTest goBangTest = new GoBangTest();
+        goBangTest.move(9,9);
+        goBangTest.printBoard();
     }
 }
