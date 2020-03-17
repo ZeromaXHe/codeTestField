f72c88a41e19d8a27890fb3a8166ccd5630f5bd4
Zhu XH <zhuxiaohe95@163.com>
2020-03-17 22:04:53 +0800

    0.0.4.4 任务计时器前端简易echarts饼图 熟悉json的转换

--- codeTestFieldMain/src/test/java/cn/zeromax/timer/TaskTimerController.java
+++ codeTestFieldMain/src/test/java/cn/zeromax/timer/TaskTimerController.java
@@ -9,12 +9,12 @@ import java.io.IOException;
 public class TaskTimerController {
     private transient String json = "";
     private TaskTimer taskTimer;
-    private final String filePath = "codeTestFieldMain\\src\\test\\java\\cn\\zeromax\\timertaskTimer.txt";
+    private final String filePath = "codeTestFieldMain\\src\\test\\java\\cn\\zeromax\\timer\\taskTimer.json";
 
     TaskTimerController() {
         File file = new File(filePath);
         if (file.exists()) {
-            System.out.println("timertaskTimer.txt文件存在，读取内容：");
+            System.out.println(file.getName() + "文件存在，读取内容：");
             json = FileUtil.readFile(filePath);
             taskTimer = JSON.parseObject(json, new TypeReference<TaskTimer>() {
             });
@@ -25,7 +25,7 @@ public class TaskTimerController {
             taskTimer = new TaskTimer();
             try {
                 file.createNewFile();
-                System.out.println("之前未创建timertaskTimer.txt文件，现在创建了，绝对路径为：" + file.getAbsolutePath());
+                System.out.println("之前未创建" + file.getName() + "文件，现在创建了，绝对路径为：" + file.getAbsolutePath());
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -42,21 +42,21 @@ public class TaskTimerController {
 
     public void save() {
         json = JSON.toJSONString(taskTimer);
-        System.out.println("正在保存json: "+json+" 至path: "+filePath);
+        System.out.println("正在保存json: " + json + " 至path: " + filePath);
         FileUtil.writeToFile(filePath, json);
     }
 
     public static void main(String[] args) {
         TaskTimerController taskTimerController = new TaskTimerController();
-        taskTimerController.startTask("test");
+        taskTimerController.startTask("测试");
         try {
-            System.out.println("开始1s休眠");
-            Thread.sleep(1000);
+            System.out.println("开始2s休眠");
+            Thread.sleep(2000);
             System.out.println("结束休眠");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        taskTimerController.endTask("test");
+        taskTimerController.endTask("测试");
         taskTimerController.save();
     }
 }
